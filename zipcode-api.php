dd1fb1b9c71948d712df8e7e3607c5900c292987
Mihai Peteu <jericho1ne@yahoo.com>
2018-08-15 18:19:27 -0700

    formatting

--- zipcode-api.php
+++ zipcode-api.php
@@ -1,10 +1,10 @@
 <?php
-  // Set up your API auth id and token
-  $auth_id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-  $auth_token = 'xxxxxxxxxxxxxxxxxxxx';
+    // Set up your API auth id and token
+    $auth_id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+    $auth_token = 'xxxxxxxxxxxxxxxxxxxx';
 
-  // Choose a state name
-  $state = "CA";
+    // Choose a state name
+    $state = "CA";
 
 	// Parse city names from the plain text file below
 	$filename = 'cities.txt';
@@ -14,8 +14,8 @@
 	// Smarty Streets curl call which returns JSON result
 	//
 	// MORE INFO:
-  // https://smartystreets.com/products/apis/us-zipcode-api
-  // 
+    // https://smartystreets.com/products/apis/us-zipcode-api
+    //
 	$ch = curl_init();
 	$http_header = [
 		"content-type: application/json",
@@ -23,28 +23,32 @@
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $http_header);
 	// Do not auto-print results!
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	
+
 	echo "\r\n" . "************************************************" . "\r\n";
-	
+
 	foreach ($cityNames as $city) {
 		// Print current City name:
-		echo $city . "\r\n"; 
-		
+		echo $city . "\r\n";
+
 		// Replace all spaces with `+` signs
 		$city =  str_replace(' ', '+', $city);
 
-		// Set URL 
-		$url = "https://us-zipcode.api.smartystreets.com/lookup?auth-id={$auth_id}&auth-token={$auth_token}&city={$city}&state={$state}";
-    curl_setopt($ch, CURLOPT_URL, $url);
-		$htmlResponse = curl_exec($ch);
-		
+        // Set URL
+        $base = "https://us-zipcode.api.smartystreets.com/lookup?";
+		$url = $base . "auth-id={$auth_id}&auth-token={$auth_token}&city={$city}&state={$state}";
+        
+        // Setup the cURL call
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $htmlResponse = curl_exec($ch);
+        
+        // Decode JSON response
 		$resultArray = json_decode($htmlResponse, true);
-    
-    foreach ($resultArray[0]['zipcodes'] as $chunk) {
-			echo($chunk['zipcode'] . ', ');
-    }
-    
-		echo "\r\n" . "************************************************" . "\r\n";
-	} // End foreach through cityNames
-?>
 
+        // Print out zipcodes associated with city name
+        foreach ($resultArray[0]['zipcodes'] as $chunk) {
+            echo($chunk['zipcode'] . ', ');
+        }
+
+		echo "\r\n" . "************************************************" . "\r\n";
+	} // End foreach loop through city names
+?>
